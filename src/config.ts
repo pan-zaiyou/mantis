063cb3288e53206e561730748cdab9f3edd11623
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-09-07 20:57:28 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,19 +5,37 @@ export const drawerWidth = 260;
 
 // ==============================|| THEME CONFIG  ||============================== //
 
+const getGA = () => {
+  if (import.meta.env.DEV) {
+    return "";
+  }
+
+  switch (window.location.hostname) {
+    case "abc.gfwnetwork.com":
+      return "G-HSJEER5NF4";
+    case "beta.v2b.rip":
+      return "G-D6GDYEWSQT";
+    default:
+      return "";
+  }
+};
+
 const config: DefaultConfigProps = {
   defaultPath: "/",
-  fontFamily: "'Roboto', sans-serif",
+  fontFamily: `'Public Sans', sans-serif`,
   miniDrawer: false,
   container: true,
   themeDirection: "ltr",
   title: "跨越长城",
   title_split: " - ",
-  background_url: "",
-  description: "slogan",
-  logo: "logourl",
+  background_url: "https://unsplash.com/random",
+  description: "天下武功 唯快不破",
+  logo: "",
   api: "https://sub.heycloud.club",
-  languages: ["en-US","zh-CN","zh-TW"],
+  languages: ["zh-CN", "en-US"],
+  googleAnalytics: {
+    measurementId: getGA()
+  },
   emojiEndpoint: "https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/{{code}}.png",
   startYear: 2019
 };
