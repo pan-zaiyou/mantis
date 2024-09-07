8334aaa9c42c9143e8da6f7540ba872ec106187e
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-09-08 00:09:33 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -1,30 +1,21 @@
 // material-ui
 import { useTheme } from '@mui/material/styles';
-// import logoDark from 'assets/images/logo-dark.svg';
-// import logo from 'assets/images/logo.svg';
-
-/**
- * if you want to use image instead of <svg> uncomment following.
- *
- * import logoDark from 'assets/images/logo-dark.svg';
- * import logo from 'assets/images/logo.svg';
- *
- */
+import logoDark from 'assets/images/logo-dark.svg';
+import logo from 'assets/images/logo.svg';
 
 // ==============================|| LOGO SVG ||============================== //
 
 const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
   const theme = useTheme();
   return (
-    /**
-     * if you want to use image instead of svg uncomment following, and comment out <svg> element.
-     *
-     * <img src={theme.palette.mode === 'dark' ? logoDark : logo} alt="Mantis" width="100" />
-     *
-     */
-    <>
-      <img src={theme.palette.mode === 'dark' ? "暗黑模式logourl" : "亮色模式logourl"} alt="MOEU" width="100" />
-    </>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img 
+        src={theme.palette.mode === 'dark' ? logoDark : logo} 
+        alt="跨越长城" 
+        width="100" 
+      />
+      <span style={{ marginLeft: '10px', fontSize: '18px', fontWeight: 'bold' }}>MOEU</span>
+    </div>
   );
 };
 
