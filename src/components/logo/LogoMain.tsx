c0f50b889bf5f85f714cb727c9f66b56d04b32c9
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-09-08 12:49:43 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -10,7 +10,7 @@ const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
         height="43"
         viewBox="0 0 12 12"
         xmlns="http://www.w3.org/2000/svg"
-        style={{ marginRight: '4px', verticalAlign: 'middle' }}  {/* 这里添加 verticalAlign 确保图标与字体在同一行 */}
+        style={{ marginRight: '4px', verticalAlign: 'middle' }}  {/* 确保图标与字体在同一行 */}
       >
         <path
           d="M8.024 2.52 12 6.496l-1.353 1.356-2.623-2.623-.922.926-1.357-1.356zM5.327 5.219 6.68 6.576l1.273 1.269-1.357 1.353-2.619-2.619-2.088 2.088-.535.531L0 7.846 3.976 3.87z"
