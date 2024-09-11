5189e36501c932dd0fed5addb7b7996151958dd4
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-09-12 02:18:12 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -5,7 +5,7 @@ const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
 
   return (
     <div style={{ display: 'flex', alignItems: 'center', ...others }}>
-      <svg width="40" height="40" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '4px', verticalAlign: 'middle' }}>
+      <svg width="40" height="40" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '4px', marginTop: '2px' }}>
         <path
           d="M8.024 2.52 12 6.496l-1.353 1.356-2.623-2.623-.922.926-1.357-1.356zM5.327 5.219 6.68 6.576l1.273 1.269-1.357 1.353-2.619-2.619-2.088 2.088-.535.531L0 7.846 3.976 3.87z"
           fill={theme.palette.mode === 'dark' || reverse ? theme.palette.common.white : theme.palette.common.black}
