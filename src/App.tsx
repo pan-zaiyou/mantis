79a76277cdd3352222b8075476028d0d8bfbc63f
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2025-02-25 22:21:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // third-party
 import { I18nextProvider } from "react-i18next";
 import { SnackbarProvider } from "notistack";
 import { CacheProvider } from "@emotion/react";
-
-// material-ui
-import { GlobalStyles } from "tss-react";
 import { useTheme } from "@mui/material/styles";
 
 // project import
 import Routes from "@/routes";
 import ThemeCustomization from "@/themes";
-// import RTLLayout from '@/components/RTLLayout';
 import ScrollTop from "@/components/ScrollTop";
 import cache from "@/themes/cache";
 import i18n from "@/i18n";
@@ -20,28 +16,36 @@ import usePageAnalyticsEffect from "@/hooks/usePageAnalyticsEffect";
 import useAuthStateDetector from "@/hooks/useAuthStateDetector";
 import useHtmlLangSelector from "@/hooks/useHtmlLangSelector";
 import useTitle from "@/hooks/useTitle";
+import { useAuth } from "@/contexts/AuthContext"; // 导入 AuthContext
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
 
 const App = () => {
   const theme = useTheme();
+  const { user } = useAuth(); // 获取用户信息
 
   usePageAnalyticsEffect();
   useAuthStateDetector();
   useHtmlLangSelector();
   useTitle(null);
 
+  // 在用户登录时更新 Crisp 用户信息
+  useEffect(() => {
+    if (user) {
+      window.$crisp.push(["set", "user", [user.email]]); // 设置 Crisp 用户信息
+    }
+  }, [user]);
+
   return (
     <CacheProvider value={cache}>
       <ThemeCustomization>
-        {/* <RTLLayout> */}
         <I18nextProvider i18n={i18n}>
           <ScrollTop>
             <SnackbarProvider
               maxSnack={3}
               anchorOrigin={{
                 vertical: "top",
-                horizontal: "center"
+                horizontal: "center",
               }}
               autoHideDuration={4000}
               dense
@@ -52,15 +56,14 @@ const App = () => {
                   body: {
                     transition: theme.transitions.create("background-color", {
                       duration: theme.transitions.duration.shortest,
-                      easing: theme.transitions.easing.easeInOut
-                    })
-                  }
+                      easing: theme.transitions.easing.easeInOut,
+                    }),
+                  },
                 }}
               />
             </SnackbarProvider>
           </ScrollTop>
         </I18nextProvider>
-        {/* </RTLLayout> */}
       </ThemeCustomization>
     </CacheProvider>
   );
