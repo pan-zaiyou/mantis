1750c9f4ae6f9e4820bf9972f0ca6a0fa2a8217a
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2026-04-12 17:58:15 +0800

    Update useAuthStateDetector.ts

--- src/hooks/useAuthStateDetector.ts
+++ src/hooks/useAuthStateDetector.ts
@@ -1,4 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import lo from "lodash-es";
+
+// project imports
 import { useDispatch, useSelector } from "@/store";
 import { useGetUserInfoQuery } from "@/store/services/api";
 import { logout } from "@/store/reducers/auth";
@@ -6,39 +9,23 @@ import { logout } from "@/store/reducers/auth";
 const useAuthStateDetector = () => {
   const dispatch = useDispatch();
   const isLogin = useSelector((state) => state.auth.isLoggedIn);
+  const { error } = useGetUserInfoQuery(undefined, {
+    skip: !isLogin
+  });
 
-  const { data, error } = useGetUserInfoQuery(undefined, { skip: !isLogin });
-  const hasBindUser = useRef(false);
-
-  // 错误处理逻辑
   useEffect(() => {
-    if (error) {
+    if (!lo.isEmpty(error)) {
       console.error(error);
-      if (error.status === 401 || error.status === 403) {
-        dispatch(logout());
+
+      if (lo.isNumber((error as any).status)) {
+        switch ((error as any).status) {
+          case 401:
+          case 403:
+            dispatch(logout());
+        }
       }
     }
-  }, [error, dispatch]);
-
-  // 绑定已登录用户邮箱
-  useEffect(() => {
-    const user = data?.data;
-    if (!user?.email) return;
-    if (hasBindUser.current) return;
-
-    hasBindUser.current = true;
-
-    const bindUser = () => {
-      if (window.$crisp) {
-        window.$crisp.push(["set", "user:email", [user.email]]);
-        window.$crisp.push(["set", "user:nickname", [user.email]]);
-      } else {
-        setTimeout(bindUser, 300);
-      }
-    };
-
-    bindUser();
-  }, [data]);
+  }, [error]);
 
   return isLogin;
 };
