10b77b80da4d41787dbc8b5cff790be67e764793
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2026-04-12 21:00:00 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,66 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 
-import { CacheProvider } from "@emotion/react";
+// third-party
 import { I18nextProvider } from "react-i18next";
 import { SnackbarProvider } from "notistack";
+import { CacheProvider } from "@emotion/react";
+
+// material-ui
+import { GlobalStyles } from "tss-react";
 import { useTheme } from "@mui/material/styles";
 
-import AppRoutes from "@/routes";
+// project import
+import Routes from "@/routes";
 import ThemeCustomization from "@/themes";
+// import RTLLayout from '@/components/RTLLayout';
 import ScrollTop from "@/components/ScrollTop";
-import i18n from "@/i18n";
 import cache from "@/themes/cache";
+import i18n from "@/i18n";
+import usePageAnalyticsEffect from "@/hooks/usePageAnalyticsEffect";
 import useAuthStateDetector from "@/hooks/useAuthStateDetector";
 import useHtmlLangSelector from "@/hooks/useHtmlLangSelector";
 import useTitle from "@/hooks/useTitle";
 
+// ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
+
 const App = () => {
   const theme = useTheme();
-  const isLogin = useAuthStateDetector();
+
+  usePageAnalyticsEffect();
+  useAuthStateDetector();
   useHtmlLangSelector();
   useTitle(null);
-  const location = useLocation();
-
-  // 页面状态同步给 Crisp
-  useEffect(() => {
-    if (window.$crisp) {
-      window.$crisp.push([
-        "set",
-        "session:current_page",
-        [window.location.href],
-      ]);
-    }
-  }, [location.pathname]);
 
   return (
     <CacheProvider value={cache}>
       <ThemeCustomization>
+        {/* <RTLLayout> */}
         <I18nextProvider i18n={i18n}>
           <ScrollTop>
             <SnackbarProvider
               maxSnack={3}
-              anchorOrigin={{ vertical: "top", horizontal: "center" }}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "center"
+              }}
               autoHideDuration={4000}
               dense
             >
-              <AppRoutes />
+              <Routes />
+              <GlobalStyles
+                styles={{
+                  body: {
+                    transition: theme.transitions.create("background-color", {
+                      duration: theme.transitions.duration.shortest,
+                      easing: theme.transitions.easing.easeInOut
+                    })
+                  }
+                }}
+              />
             </SnackbarProvider>
           </ScrollTop>
         </I18nextProvider>
+        {/* </RTLLayout> */}
       </ThemeCustomization>
     </CacheProvider>
   );
