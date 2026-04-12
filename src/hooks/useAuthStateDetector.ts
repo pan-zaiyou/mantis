7b35061a22852a3a81a6da52e0f8807692685223
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2026-04-12 20:59:22 +0800

    Update useAuthStateDetector.ts

--- src/hooks/useAuthStateDetector.ts
+++ src/hooks/useAuthStateDetector.ts
@@ -1,45 +1,31 @@
 import { useEffect } from "react";
+import lo from "lodash-es";
+
+// project imports
 import { useDispatch, useSelector } from "@/store";
 import { useGetUserInfoQuery } from "@/store/services/api";
 import { logout } from "@/store/reducers/auth";
 
-let crispBound = false; // 全局锁，保证只绑定一次邮箱
-
 const useAuthStateDetector = () => {
   const dispatch = useDispatch();
   const isLogin = useSelector((state) => state.auth.isLoggedIn);
-
-  const { data, error } = useGetUserInfoQuery(undefined, {
+  const { error } = useGetUserInfoQuery(undefined, {
     skip: !isLogin
   });
 
-  // 处理401/403错误
   useEffect(() => {
-    if (error) {
+    if (!lo.isEmpty(error)) {
       console.error(error);
-      if (error.status === 401 || error.status === 403) {
-        dispatch(logout());
+
+      if (lo.isNumber((error as any).status)) {
+        switch ((error as any).status) {
+          case 401:
+          case 403:
+            dispatch(logout());
+        }
       }
     }
-  }, [error, dispatch]);
-
-  // 绑定 Crisp 用户邮箱
-  useEffect(() => {
-    const user = data?.data;
-    if (!user?.email) return;
-    if (crispBound) return;
-
-    const timer = setInterval(() => {
-      if (window.$crisp) {
-        window.$crisp.push(["set", "user:email", [user.email]]);
-        window.$crisp.push(["set", "user:nickname", [user.email]]);
-        crispBound = true;
-        clearInterval(timer);
-      }
-    }, 300);
-
-    return () => clearInterval(timer);
-  }, [data]);
+  }, [error]);
 
   return isLogin;
 };
